f55631d0e8cfcb77ce4c854bf1af14d5c5471f5b
Yernazar Mukametali <yernazarmukametali@MacBook-Pro-Yernazar.local>
2025-12-04 11:09:57 +0500

    added fonts,icons,images,colors

---
+++ lib/app/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color green = Color(0xFF43D049);
+  static const Color red = Color(0xFFEB5757);
+  static const Color darkBlue = Color(0xFF0B1E2D);
+  static const Color blue = Color(0xFF22A2BD);
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+void main(List<String> args) {}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp();
+  }
+}
